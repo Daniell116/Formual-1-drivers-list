181587047d56892c456a4c5b9968734abf554363
daniell escareno <daniellescareno7@gmail.com>
2026-03-09 08:39:51 -0700

    Initial Commit

---
+++ Formual 1 drivers list/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Formual 1 drivers list
+//
+//  Created by daniell Escareno on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Formual 1 drivers list/Formual_1_drivers_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Formual_1_drivers_listApp.swift
+//  Formual 1 drivers list
+//
+//  Created by daniell Escareno on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Formual_1_drivers_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
